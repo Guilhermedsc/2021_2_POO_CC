a51c381ddb7f75101c271d9e80e2e8501811f91b
Guilherme <84539587+Guilhermedsc@users.noreply.github.com>
2021-11-30 14:14:23 -0300

    agenda 2

--- Busca/main.cpp
+++ Busca/main.cpp
@@ -23,7 +23,7 @@ class CONTATO{
     vector<FONE> fones;
     string nome;
 
-    //bool
+    //bool-
     int validarNumero(string number){
         int temLetra;
         for(int i=0; i<number.size(); i++){
